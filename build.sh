81f1a13512d82afcd1d66a7477daa4572ee9d482
CLAIRESKYLIGHT <bioangela3@gmail.com>
2025-05-14 16:06:01 +0800

    Add build script for Render deployment

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install PHP and required extensions
+apt-get update
+apt-get install -y software-properties-common
+add-apt-repository ppa:ondrej/php
+apt-get update
+apt-get install -y php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-mbstring php8.2-xml php8.2-zip php8.2-mysql
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install dependencies
+composer install --optimize-autoloader --no-dev
+php artisan key:generate
+php artisan migrate --force
+
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
+
+# Install frontend dependencies and build
+npm install
+npm run build 
